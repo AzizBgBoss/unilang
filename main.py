d6661ba0598a3eceab2e6a69b66ab6b708a7608e
AzizBgBoss <mohamedazizbenghezala64@gmail.com>
2026-08-20 21:26:20 +0100

    added graphics (64x64@1)

--- main.py
+++ main.py
@@ -7,10 +7,22 @@ Tips:
 - Sizes are specified in bits
 - 1 bit variables are good for boolean values, 8 bit variables are good for characters, and 32 bit variables are good for integers
 - 2-bit ranges are 0-3, 4-bit ranges are 0-15, 8-bit ranges are 0-255, 16-bit ranges are 0-65535, and 32-bit ranges are 0-4294967295
+- flags are special 1-bit variables that can be used for custom features
+- flag 0 (graphics) (this will consume the last n bits of memory):
+0 - disable graphics
+1 - enable monochrome graphics (64x64) (1 x 64 x 64 = 4096 bits = 512 bytes)
+2 - enable 4-shade grayscale graphics (64x64) (4 x 64 x 64 = 16384 bits = 2048 bytes)
+3 - enable 8-bit color graphics (64x64) (8 x 64 x 64 = 32768 bits = 4096 bytes)
+4 - enable monochrome graphics (128x64) (1 x 128 x 64 = 8192 bits = 1024 bytes)
+5 - enable 4-shade grayscale graphics (128x64) (4 x 128 x 64 = 32768 bits = 4096 bytes)
+6 - enable 8-bit color graphics (128x64) (8 x 128 x 64 = 65536 bits = 8192 bytes)
 
 Commands:
 #...# - comment (stops until the next #)
-mem[addr:size]:val - set the value of a memory address with a certain size
+flag[n]=val - set the value of a flag with a certain index to a 4-bit value
+isflagsupported(n, val, addr:size) - check if a flag with a certain index is supported (changes based on platforms)
+mem[addr:size]=val - set the value of a memory address with a certain size
+setmem(addr1:size1, addr2:size2) - set the value of a memory address with a certain size to the value of another memory address with a certain size
 outc(addr:size) - output the character of a memory address with a certain size
 out(addr:size) - output the value of a memory address with a certain size
 input(chars, addr:size) - read a certain amount of characters and store them in memory
@@ -31,6 +43,7 @@ mod(val, addr1:size1, addr2:size2) - get the modulus of a memory address with a 
 modv(addr1:size1, addr2:size2, addr3:size3) - get the modulus of two memory addresses with certain sizes and store it in another memory address with a certain size (you can use the same memory address for both the input and output)
 cur - print the current position in the program
 memory - print the current state of memory
+flags - print the current state of flags
 setcur(pos) - set the current position in the program
 compare(val, addr1:size1, addr2:size2) - compare a value to a memory address with a certain size and store the result in a special memory address (0 = equal, 1 = val > addr2, 2 = val < addr2)
 comparev(addr1:size1, addr2:size2, addr3:size3) - compare two memory addresses with certain sizes and store the result in a special memory address (0 = equal, 1 = addr1 > addr2, 2 = addr1 < addr2)
@@ -58,17 +71,13 @@ import random
 import sys
 import time
 
-memsize = 4096
-mem = bytearray(memsize // 8)
+memsize = 1024 * 4
+supportedFlags = [[0] * 16] * 16
 
-def format_size(n, suffix="B"):
-    for unit in ["", "K", "M", "G", "T"]:
-        if abs(n) < 1024:
-            return f"{n:3.1f}{unit}{suffix}"
-        n /= 1024
-    return f"{n:.1f}P{suffix}"
+supportedFlags[0][0] = 1 # disabling graphics is ofc supported
+supportedFlags[0][1] = 1 # 64x64@1
 
-print(f"Starting unilang interpreter with {format_size(memsize)} bytes of memory...\n")
+print(f"Starting unilang interpreter with {memsize} bits of memory ({memsize // 8} bytes)...\n")
 
 def get_bit(mem, addr):
     return (mem[addr // 8] >> (addr % 8)) & 1
@@ -117,17 +126,36 @@ if len(sys.argv) > 1 and sys.argv[1] == "-f":
 else:
     program = input(">")
 
-print(program)
+print(program + '\n')
 
 cutoffs = "[];\n()"
 
 running = True
 cur = 0
 tok = ""
+mem = bytearray(memsize // 8)
+flags = bytearray(16 * 16 // 8)
 
 nextreturn = 0
 
+import tkinter as tk
+
+def on_close():
+    global running
+    running = False
+
 while running and cur < len(program):
+    if get_val(flags, 0, 4) == 1:
+        try:
+            root.update()
+        except tk.TclError:
+            running = False
+            break
+        canvas.delete("all")
+        for x in range(64):
+            for y in range(64):
+                if get_bit(mem, memsize - x * 64 - y - 1):
+                    canvas.create_rectangle(x*10, y*10, x*10+10, y*10+10, fill="white", outline="")
     if program[cur] == "#":
         tok = readuntil(program, cur, "#")
         cur += len(tok) + 1
@@ -137,6 +165,54 @@ while running and cur < len(program):
     if tok.strip() == "":
         cur += 1
         continue
+    if tok == "flag":
+        cur += 1
+        tok = readuntil(program, cur, "]")
+        cur += len(tok)
+        flag = int(tok)
+
+        cur += 2
+        tok = readuntil(program, cur, ";\n")
+        cur += len(tok)
+        val = int(tok)
+
+        cur += 2
+
+        if flag == 0:
+            if val == 1:
+                if get_val(flags, flag * 4, 4) == 0:
+                    root = tk.Tk()
+                    root.protocol("WM_DELETE_WINDOW", lambda: on_close())
+                    canvas = tk.Canvas(root, width=64*10, height=64*10, bg="black")
+                    canvas.pack()
+            if val == 0:
+                if get_val(flags, flag * 4, 4) != 0:
+                    root.destroy()
+        set_val(flags, flag * 4, val, 4)
+    if tok == "isflagsupported":
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        flag = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        val = int(tok) 
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ")")
+        cur += len(tok)
+        size = int(tok)
+
+        cur += 2
+
+        set_val(mem, addr, supportedFlags[flag][val], size)
     if tok == "mem":
         cur += 1
         tok = readuntil(program, cur, ":")
@@ -156,6 +232,30 @@ while running and cur < len(program):
         cur += 2
         
         set_val(mem, addr, val, size)
+    if tok == "setmem":
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr1 = int(tok)
+        
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        size1 = int(tok)
+        
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr2 = int(tok)
+        
+        cur += 1
+        tok = readuntil(program, cur, ")")
+        cur += len(tok)
+        size2 = int(tok)
+        
+        cur += 2
+        
+        set_val(mem, addr1, get_val(mem, addr2, size2), size1)
     if tok == "outc":
         cur += 1
         tok = readuntil(program, cur, ":")
@@ -600,6 +700,10 @@ while running and cur < len(program):
     if tok == "memory":
         for i in range(memsize):
             print(get_bit(mem, i), end = "")
+        print()
+    if tok == "flags":
+        for i in range(16 * 16 * 4):
+            print(get_bit(flags, i), end = "")
         print()
     if tok == "setcur":
         cur += 1
