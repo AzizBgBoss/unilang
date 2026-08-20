0ca7b7eadda5c2c208b4fb2490ce483f0789ebd0
AzizBgBoss <mohamedazizbenghezala64@gmail.com>
2026-08-20 19:52:17 +0100

    more poopies

--- main.py
+++ main.py
@@ -2,16 +2,33 @@
 unilang by AzizBgBoss
 https://github.com/AzizBgBoss/unilang
 
+Tips:
+- Use # to add comments that are ignored by the interpreter
+- Sizes are specified in bits
+- 1 bit variables are good for boolean values, 8 bit variables are good for characters, and 32 bit variables are good for integers
+- 2-bit ranges are 0-3, 4-bit ranges are 0-15, 8-bit ranges are 0-255, 16-bit ranges are 0-65535, and 32-bit ranges are 0-4294967295
+
 Commands:
+#...# - comment (stops until the next #)
 mem[addr:size]:val - set the value of a memory address with a certain size
 outc(addr:size) - output the character of a memory address with a certain size
 out(addr:size) - output the value of a memory address with a certain size
-input(chars:addr:size) - read a certain amount of characters and store them in memory
+input(chars, addr:size) - read a certain amount of characters and store them in memory
 print("text") - print text
-printv(chars:addr:size) - print a certain amount of characters from memory
+printn("text") - print text without a newline
+printv(chars, addr:size) - print a certain amount of characters from memory
+printvn(chars, addr:size) - print a certain amount of characters from memory without a newline
 rand(addr:size) - set a memory address with a certain size to a random value
 add(val, addr1:size1, addr2:size2) - add a value to a memory address with a certain size and store it in another memory address with a certain size (you can use the same memory address for both the input and output)
 addv(addr1:size1, addr2:size2, addr3:size3) - add two memory addresses with certain sizes and store it in another memory address with a certain size (you can use the same memory address for both the input and output)
+sub(val, addr1:size1, addr2:size2) - subtract a value from a memory address with a certain size and store it in another memory address with a certain size (you can use the same memory address for both the input and output)
+subv(addr1:size1, addr2:size2, addr3:size3) - subtract two memory addresses with certain sizes and store it in another memory address with a certain size (you can use the same memory address for both the input and output)
+mul(val, addr1:size1, addr2:size2) - multiply a value with a memory address with a certain size and store it in another memory address with a certain size (you can use the same memory address for both the input and output)
+mulv(addr1:size1, addr2:size2, addr3:size3) - multiply two memory addresses with certain sizes and store it in another memory address with a certain size (you can use the same memory address for both the input and output)
+div(val, addr1:size1, addr2:size2) - divide a memory address with a certain size by a value and store it in another memory address with a certain size (you can use the same memory address for both the input and output)
+divv(addr1:size1, addr2:size2, addr3:size3) - divide two memory addresses with certain sizes and store it in another memory address with a certain size (you can use the same memory address for both the input and output)
+mod(val, addr1:size1, addr2:size2) - get the modulus of a memory address with a certain size by a value and store it in another memory address with a certain size (you can use the same memory address for both the input and output)
+modv(addr1:size1, addr2:size2, addr3:size3) - get the modulus of two memory addresses with certain sizes and store it in another memory address with a certain size (you can use the same memory address for both the input and output)
 cur - print the current position in the program
 memory - print the current state of memory
 setcur(pos) - set the current position in the program
@@ -31,13 +48,25 @@ subroutine(cur) - call a subroutine at a certain position in the program
 return - return from a subroutine
 if(addr:size, cur) - if the value of a memory address with a certain size is not 0, jump to a certain position in the program
 ifsubroutine(addr:size, cur) - if the value of a memory address with a certain size is not 0, call a subroutine at a certain position in the program
+sleep(ms) - sleep for a certain amount of milliseconds
 exit - exit the program
 '''
 
 import random
+import sys
+import time
 
-memsize = 16
+memsize = 4096
 mem = bytearray(memsize // 8)
+
+def format_size(n, suffix="B"):
+    for unit in ["", "K", "M", "G", "T"]:
+        if abs(n) < 1024:
+            return f"{n:3.1f}{unit}{suffix}"
+        n /= 1024
+    return f"{n:.1f}P{suffix}"
+
+print(f"Starting unilang interpreter with {format_size(memsize)} bytes of memory...\n")
 
 def get_bit(mem, addr):
     return (mem[addr // 8] >> (addr % 8)) & 1
@@ -62,7 +91,7 @@ def get_val(mem, addr, size):
 def readuntil(program, cur, stop):
     tok = ""
     while cur < len(program):
-        if program[cur] != stop:
+        if program[cur] not in stop:
             tok += program[cur]
             cur += 1
         else:
@@ -79,13 +108,16 @@ def readuntilcutoff(program, cur):
             return tok
     return tok
 
-program = input(">")
-program = program.replace(" ", "")
-program = program.replace("\n", ";")
+# check for -f flag in command line arguments
+if len(sys.argv) > 1 and sys.argv[1] == "-f":
+    with open(sys.argv[2], "r") as f:
+        program = f.read()
+else:
+    program = input(">")
 
 print(program)
 
-cutoffs = "[];()"
+cutoffs = "[];\n()"
 
 running = True
 cur = 0
@@ -94,8 +126,15 @@ tok = ""
 nextreturn = 0
 
 while running and cur < len(program):
+    if program[cur] == "#":
+        tok = readuntil(program, cur, "#")
+        cur += len(tok) + 1
+        continue
     tok = readuntilcutoff(program, cur)
     cur += len(tok)
+    if tok.strip() == "":
+        cur += 1
+        continue
     if tok == "mem":
         cur += 1
         tok = readuntil(program, cur, ":")
@@ -108,11 +147,11 @@ while running and cur < len(program):
         size = int(tok)
         
         cur += 2
-        tok = readuntil(program, cur, ";")
+        tok = readuntil(program, cur, ";\n")
         cur += len(tok)
         val = int(tok)
         
-        cur += 1
+        cur += 2
         
         set_val(mem, addr, val, size)
     if tok == "outc":
@@ -128,7 +167,7 @@ while running and cur < len(program):
         
         cur += 2
         
-        print(chr(get_val(mem, addr, size)))
+        print(chr(get_val(mem, addr, size)), end = "")
     if tok == "out":
         cur += 1
         tok = readuntil(program, cur, ":")
@@ -142,7 +181,7 @@ while running and cur < len(program):
         
         cur += 2
         
-        print(get_val(mem, addr, size))
+        print(get_val(mem, addr, size), end = "")
     if tok == "input":
         cur += 1
         tok = readuntil(program, cur, ",")
@@ -171,6 +210,13 @@ while running and cur < len(program):
         print(tok)
 
         cur += 3
+    if tok == "printn":
+        cur += 2
+        tok = readuntil(program, cur, "\"")
+        cur += len(tok)
+        print(tok, end = "")
+
+        cur += 3
     if tok == "printv":
         cur += 1
         tok = readuntil(program, cur, ",")
@@ -187,11 +233,31 @@ while running and cur < len(program):
         cur += len(tok)
         size = int(tok)
 
-        cur += 1
+        cur += 2
 
         for i in range(chars):
             print(chr(get_val(mem, addr + i * size, size)), end = "")
         print()
+    if tok == "printvn":
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        chars = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ")")
+        cur += len(tok)
+        size = int(tok)
+
+        cur += 2
+
+        for i in range(chars):
+            print(chr(get_val(mem, addr + i * size, size)), end = "")
     if tok == "rand":
         cur += 1
         tok = readuntil(program, cur, ":")
@@ -203,7 +269,7 @@ while running and cur < len(program):
         cur += len(tok)
         size = int(tok)
 
-        cur += 1
+        cur += 2
 
         set_val(mem, addr, random.randint(0, 2 ** size - 1), size)
     if tok == "add":
@@ -269,6 +335,264 @@ while running and cur < len(program):
         cur += 2
 
         set_val(mem, addr3, get_val(mem, addr1, size1) + get_val(mem, addr2, size2), size3)
+    if tok == "sub":
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        val = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        size1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr2 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ")")
+        cur += len(tok)
+        size2 = int(tok)
+
+        cur += 2
+
+        set_val(mem, addr2, get_val(mem, addr1, size1) - val, size2)
+    if tok == "subv":
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        size1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr2 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        size2 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr3 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ")")
+        cur += len(tok)
+        size3 = int(tok)
+
+        cur += 2
+
+        set_val(mem, addr3, get_val(mem, addr1, size1) - get_val(mem, addr2, size2), size3)
+    if tok == "mul":
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        val = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        size1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr2 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ")")
+        cur += len(tok)
+        size2 = int(tok)
+
+        cur += 2
+
+        set_val(mem, addr2, val * get_val(mem, addr1, size1), size2)
+    if tok == "mulv":
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        size1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr2 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        size2 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr3 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ")")
+        cur += len(tok)
+        size3 = int(tok)
+
+        cur += 2
+
+        set_val(mem, addr3, get_val(mem, addr1, size1) * get_val(mem, addr2, size2), size3)
+    if tok == "div":
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        val = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        size1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr2 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ")")
+        cur += len(tok)
+        size2 = int(tok)
+
+        cur += 2
+
+        if val != 0:
+            set_val(mem, addr2, get_val(mem, addr1, size1) // val, size2)
+    if tok == "divv":
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        size1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr2 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        size2 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr3 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ")")
+        cur += len(tok)
+        size3 = int(tok)
+
+        cur += 2
+
+        val2 = get_val(mem, addr2, size2)
+        if val2 != 0:
+            set_val(mem, addr3, get_val(mem, addr1, size1) // val2, size3)
+    if tok == "mod":
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        val = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        size1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr2 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ")")
+        cur += len(tok)
+        size2 = int(tok)
+
+        cur += 2
+
+        if val != 0:
+            set_val(mem, addr2, get_val(mem, addr1, size1) % val, size2)
+    if tok == "modv":
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        size1 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr2 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ",")
+        cur += len(tok)
+        size2 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ":")
+        cur += len(tok)
+        addr3 = int(tok)
+
+        cur += 1
+        tok = readuntil(program, cur, ")")
+        cur += len(tok)
+        size3 = int(tok)
+
+        cur += 2
+
+        val2 = get_val(mem, addr2, size2)
+        if val2 != 0:
+            set_val(mem, addr3, get_val(mem, addr1, size1) % val2, size3)
     if tok == "cur":
         print(cur)
     if tok == "memory":
@@ -754,5 +1078,14 @@ while running and cur < len(program):
         if get_val(mem, addr, size) != 0:
             nextreturn = cur
             cur = target
+    if tok == "sleep":
+        cur += 1
+        tok = readuntil(program, cur, ")")
+        cur += len(tok)
+        ms = int(tok)
+
+        cur += 2
+
+        time.sleep(ms / 1000)
     if tok == "exit":
         running = False
